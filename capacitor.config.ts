59d5b5516f4036fe10cb3861e60985cb839e096a
Douglas White <email@douglaswhite.dev>
2024-12-21 12:35:13 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'prophecy-study',
+  webDir: 'www'
+};
+
+export default config;
